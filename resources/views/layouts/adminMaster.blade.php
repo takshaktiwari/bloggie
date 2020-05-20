48759311a8f3b0c9d8c700a8382cbe108b5d35a3
master <master@email.com>
2020-05-20 12:09:04 +0530

    moved to jquery 3.5, many libs folders are deleted

--- resources/views/layouts/adminMaster.blade.php
+++ resources/views/layouts/adminMaster.blade.php
@@ -18,7 +18,10 @@
     
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css">
     
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script
+        src="https://code.jquery.com/jquery-3.5.1.min.js"
+        integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
+        crossorigin="anonymous"></script>
     <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
     <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js"></script>
     
--- resources/views/layouts/frontMaster.blade.php
+++ resources/views/layouts/frontMaster.blade.php
@@ -197,7 +197,10 @@
     <!-- ================================================== -->
 
     @section('scripts')
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script
+              src="https://code.jquery.com/jquery-3.5.1.min.js"
+              integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
+              crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
         <script>
             $(document).ready(function(){
